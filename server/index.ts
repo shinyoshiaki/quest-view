e94004fdddd8456aa6260ec82c1202fffd03b0fe
DESKTOP-MQ2AAOT\shin <shinyoshiaki2012@gmail.com>
2019-05-26 14:59:35 +0900

    websocket works

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,12 @@
-import ws from 'ws'
+import WS from "ws";
+
+console.log("server")
+
+const wss = new WS.Server({ port: 8080 });
+
+wss.on("connection", ws => {
+  ws.on("message", v => {
+    console.log(v);
+  });
+  ws.send("server");
+});
--- server/lib/index.js
+++ server/lib/index.js
@@ -1,2 +1,13 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const ws_1 = tslib_1.__importDefault(require("ws"));
+console.log("server");
+const wss = new ws_1.default.Server({ port: 8080 });
+wss.on("connection", ws => {
+    ws.on("message", v => {
+        console.log(v);
+    });
+    ws.send("server");
+});
 //# sourceMappingURL=index.js.map
