c0f78342940e0c00e821da4ca377bd8a3821d08a
DESKTOP-MQ2AAOT\shin <shinyoshiaki2012@gmail.com>
2019-06-24 00:36:15 +0900

    rm

--- capture/src/renderer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import server from "./server";
-
-server();
-
-ReactDOM.render(<App />, document.getElementById("app"));
